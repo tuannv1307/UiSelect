6726e3d578d5eff8e86edff127271c003da429b0
tuannv1307 <nvt13071@gmail.com>
2023-01-11 16:36:18 +0700

    update

--- src/components/SelectOptions/SelectOptions.tsx
+++ src/components/SelectOptions/SelectOptions.tsx
@@ -24,8 +24,8 @@ import {
 import { dataUiSelect } from "../../../constants";
 import FilterOptions from "../FilterOptions";
 import Options from "../Options";
-import { st, classes } from "./SelectOptions.st.css";
 import { arrdataRecursive, flatArrData } from "../../ultils";
+import { st, classes } from "./SelectOptions.st.css";
 
 export type SelectOptionsProps = {
   typeRender?: "single" | "tree";
